c15e1dff33d10b8dcd42bc8f1ff50d94036f64c7
Artem Leyman <a.leyman@tcsbank.ru>
2025-01-28 14:28:04 +0300

    Убрал хук oninit, перенес проверку searcherm на null в сервис, итоговое дз

--- src/app/service/users-service.ts
+++ src/app/service/users-service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
-import { UserDescription } from '../user-description';
-import { Observable, delay, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import {MOCK_USERS} from "./mock-users";
+import { Injectable } from "@angular/core";
+import { UserDescription } from "../user-description";
+import { Observable, delay, of } from "rxjs";
+import { map } from "rxjs/operators";
+import { MOCK_USERS } from "./mock-users";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class UsersService {
-  getUserList(searchTerm: string): Observable<UserDescription[]> {
+  getUserList(searchTerm: string | null): Observable<UserDescription[]> {
     return of(MOCK_USERS).pipe(
-      delay(1000),
-      map((users) =>
-        users.filter((user) =>
-          `${user.name} ${user.lastName}`
-            .toLowerCase()
-            .includes(searchTerm.toLowerCase())
-        )
-      )
+        delay(1000),
+        map((users) => {
+          if (!searchTerm) {
+            return users;
+          }
+          return users.filter((user) =>
+              `${user.name} ${user.lastName}`
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase())
+          );
+        })
     );
   }
 
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {UsersService} from "../service/users-service";
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { UsersService } from "../service/users-service";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormControl } from "@angular/forms";
 import {
   Observable,
   debounceTime,
@@ -10,30 +10,25 @@ import {
   map,
   startWith,
   switchMap,
-} from 'rxjs';
-import { UserDescription } from '../user-description';
+} from "rxjs";
 
 @Component({
-  selector: 'app-user-list',
+  selector: "app-user-list",
   standalone: true,
   imports: [CommonModule, RouterModule, ReactiveFormsModule],
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css'],
+  templateUrl: "./user-list.component.html",
+  styleUrls: ["./user-list.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UserListComponent implements OnInit {
-  userList$?: Observable<UserDescription[]>;
+export class UserListComponent {
+  readonly searchControl = new FormControl<string>("");
 
-  readonly searchControl = new FormControl<string>('');
-
-  constructor(private readonly usersService: UsersService) {}
-
-  ngOnInit() {
-    this.userList$ = this.searchControl.valueChanges.pipe(
-      startWith(''),
+  readonly userList$ = this.searchControl.valueChanges.pipe(
+      startWith(""),
       debounceTime(300),
       distinctUntilChanged(),
-      switchMap((searchTerm) => this.usersService.getUserList(searchTerm ?? ''))
-    );
-  }
+      switchMap((searchTerm) => this.usersService.getUserList(searchTerm))
+  );
+
+  constructor(private readonly usersService: UsersService) {}
 }
